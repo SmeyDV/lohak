61723ee383067d9e5b96373a50524093f5edd2a2
SmeyDeveloper_X <reaksmeyrin15@gmail.com>
2024-07-24 12:52:21 +0700

    reduce css and js code

--- js/script.js
+++ js/script.js
@@ -1,36 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   const images = [
-    'https://t4.ftcdn.net/jpg/01/02/53/49/360_F_102534906_tiedDFfDfgpIRwF7dPSU4zV2NvDtjh0S.jpg',
-    'https://www.thefairtradevillage.com/wp-content/uploads/2019/06/FTV-Ceramics-Product_AHA-7323.jpg',
-    'https://www.aptouring.com.au/-/media/travelmarvel-responsive-website/people/people-16-9/gi-t-as-cambodia-apsara-dancers-5177220-d-16-9.jpg',
-    'https://satcha-handicraft.com/wp-content/uploads/2022/12/IMG_6059-HDR.jpg',
-    'https://satcha-handicraft.com/wp-content/uploads/2022/12/IMG_6281-HDR.jpg'
+    'pic/img1.jpg',
+    'pic/img2.jpg',
+    'pic/img3.jpg',
+    'pic/img4.jpg',
+    'pic/img5.jpg'
   ];
 
   let currentImageIndex = 0;
   const jumbotron = document.getElementById('jumbotron');
-
-  function preloadImages(callback) {
-    let loadedImages = 0;
-    images.forEach((src) => {
-      const img = new Image();
-      img.src = src;
-      img.onload = () => {
-        loadedImages++;
-        if (loadedImages === images.length) {
-          callback();
-        }
-      };
-    });
-  }
 
   function changeBackgroundImage() {
     jumbotron.style.backgroundImage = `url(${images[currentImageIndex]})`;
     currentImageIndex = (currentImageIndex + 1) % images.length;
   }
 
-  preloadImages(() => {
-    changeBackgroundImage(); // Set the initial background image immediately
-    setInterval(changeBackgroundImage, 5000); // Change background image every 5 seconds
+  images.forEach(src => {
+    const img = new Image();
+    img.src = src;
   });
+
+  changeBackgroundImage(); 
+  setInterval(changeBackgroundImage, 4000);
 });
